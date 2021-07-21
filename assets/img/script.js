103ad5724210a685f2171c5dc11bb5492380389b
Rodrigo Raiman <rodrigoraiman@gmail.com>
2021-07-20 21:30:09 -0400

    prueba viajes de chile

---
+++ assets/img/script.js
@@ -0,0 +1,22 @@
+$(function(){
+
+    $("a").click(function(event){
+      if (this.hash !== "") {
+        event.preventDefault();
+  
+        var gato = this.hash;
+  
+        $("html, body").animate({
+          scrollTop: $(gato).offset().top
+        }, 800, function(){
+          window.location.hash = gato;
+        });
+      }
+    });
+  
+    $('[data-toggle="popover"]').popover();
+  
+  });
+
+  (function() {
+       var qs,js,q,s,d=document, gi=d.getElementById, ce=d.createElement, gt=d.getElementsByTagName, id="typef_orm", b="https://embed.typeform.com/"; if(!gi.call(d,id)) { js=ce.call(d,"script"); js.id=id; js.src=b+"embed.js"; q=gt.call(d,"script")[0]; q.parentNode.insertBefore(js,q) } })()
